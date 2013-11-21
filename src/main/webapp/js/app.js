eec3a406bb962e220b0be1c5d960b93fd3b77ae7
Roberto <r.zagnoni@gmail.com>
2013-11-21 23:47:57 +0100

    first commit

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function BarHistoryListCtrl($scope) {
+  $scope.rows = [
+    {"id": "1", "symbol": "DD", "entityname":"AX","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.AX","enable":"true"},
+    {"id": "2", "symbol": "IMSP", "entityname":"SPMIB","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.SPMIB","enable":"true"},
+    {"id": "3", "symbol": "DL", "entityname":"BC","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.BC","enable":"true"},
+    {"id": "4", "symbol": "CLE", "entityname":"CL","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.CL","enable":"true"},
+    {"id": "5", "symbol": "DSX", "entityname":"ET","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.DSX","enable":"true"},
+    {"id": "6", "symbol": "DB", "entityname":"GB","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.DB","enable":"true"},
+    {"id": "7", "symbol": "USA", "entityname":"ZB","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.USA","enable":"true"},
+    {"id": "8", "symbol": "SW", "entityname":"SM","assembly":"BarHistoryLoader","typename":"BarHistoryLoader.SW","enable":"true"}
+  ];
+}
